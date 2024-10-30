10ecbf004e8edc25906a0be4468d8cf578c3507b
Azzak <azzaalmahrooqi79@gmail.com>
2024-10-30 11:13:09 +0400

    Add non-clustered indexes on Name and Rating columns in Hotel table

---
+++ SQLQuery_for_the_hotel_booking_system_DatabaseProjectPart2.sql
@@ -0,0 +1,34 @@
+﻿--                                       Database Project Part 2 
+
+-- 1. Indexing Requirements 
+
+--    • Hotel Table Indexes 
+--      ✓ Add a non-clustered index on the Name column to 
+--         optimize queries that search for hotels by name. 
+           create nonclustered index idx_hotel_name
+           on hotel (Hotel_Name)
+
+--      ✓ Add a non-clustered index on the Rating column 
+--         to speed up queries that filter hotels by rating. 
+           create nonclustered index idx_hotel_rating
+           on hotel (Rating)
+
+
+--    • Room Table Indexes 
+--      ✓ Add a clustered index on the HotelID and RoomNumber 
+--         columns to optimize room lookup within a hotel 
+
+--      ✓ Add a non-clustered index on the RoomType column 
+--         to improve searches filtering by room type. 
+
+
+
+--    • Booking Table Indexes 
+--      ✓ Add a non-clustered index on GuestID to optimize 
+--         guest-related booking searches. 
+
+--      ✓ Add a non-clustered index on the Status column to 
+--         improve filtering of bookings by status. 
+
+--      ✓ Add a composite index on RoomID, CheckInDate, and 
+--         CheckOutDate for efficient querying of booking schedules.
